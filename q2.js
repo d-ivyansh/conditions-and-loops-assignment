5c8cef7e7863db89eb562450ee68d155a9b100cb
Divyansh Agarwal <divyanshagarwal573@gmail.com>
2024-02-11 01:48:34 +0530

    add files

---
+++ q2.js
@@ -0,0 +1,17 @@
+let marks=65;
+if(marks>=90)
+{
+    console.log("you got grade A");
+}
+else if(marks>=70&&marks<90){
+    console.log("you got grade B");
+}
+else if(marks>=50&&marks<70)
+{
+    console.log("you got grade C");
+
+}
+else{
+    console.log("you got grade F");
+
+}
---
+++ q5.js
@@ -0,0 +1,16 @@
+//for printing in ascending order
+console.log("for printing in ascending order");
+let num = 1;
+while(num<=25)
+{
+    console.log(num);
+    num++;
+}
+//for printing in descending order
+console.log("for printing in descending order"); 
+let num2 = 25;
+while(num2>=1)
+{
+    console.log(num2);
+    --num2;
+}
